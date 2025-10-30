34d40cb450cdc54a81395063f49a33a77640ff70
shahzaibahmadpawar1 <shazi8402@gmail.com>
2025-10-30 06:04:22 +0500

    Force add dist folder for Vercel build

---
+++ dist/server/supabaseClient.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.supabase = void 0;
+// supabaseClient.ts
+const supabase_js_1 = require("@supabase/supabase-js");
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+// Use SERVICE_ROLE key on backend (secure) rather than anon key.
+exports.supabase = (0, supabase_js_1.createClient)(supabaseUrl, supabaseKey);
